2b834e4f954598a2b07632ca0c81450135b2c4a8
Serhii Rybak <rybaksw@gmail.com>
2023-10-05 22:39:42 +0300

    just red error

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -26,7 +26,7 @@ const Input: FC<InputProps> = ({
       <input
         id={name}
         className={cn(
-          'h-12 peer placeholder:text-transparent rounded-xl px-6 py-2 w-full text-xl border-gray_light',
+          'h-12 peer placeholder:text-gray_dark rounded-xl px-6 py-2 w-full text-xl border-gray_light',
           {
             ['pr-[52px]']: button,
             ['border-red_error']: error,
@@ -60,41 +60,17 @@ const Input: FC<InputProps> = ({
           )}
         </button>
       )}
-      <label
-        className="absolute transition-all -top-6 left-4 peer-focus:-top-6 peer-focus:left-4 peer-placeholder-shown:top-2 peer-placeholder-shown:left-6 text-base peer-focus:text-base peer-placeholder-shown:text-xl text-black peer-focus:text-black peer-placeholder-shown:text-gray_dark"
-        htmlFor={name}
-      >
-        {label}
-      </label>
       {/* {error && <p className="absolute px-4 text-red_error">{error}</p>} */}
       {error && (
         <p
           className="
-          relative
-          translate-y-3
-          px-6
-          py-1
+          absolute
+          px-1
           flex
           items-center
           gap-2
           text-red_error
-          bg-red_back
-          w-full
-          after:absolute
-          after:w-0
-          after:h-0
-          after:left-10
-          after:top-0
-          after:-translate-y-full
-          after:border-solid
-          after:border-t-0
-          after:border-r-[8px]
-          after:border-l-[8px]
-          after:border-b-[13px]
-          after:border-t-transparent
-          after:border-r-transparent
-          after:border-b-red_back
-          after:border-l-transparent"
+          w-full"
         >
           <ICONS.EXCLAMATION_MARK width="14px" height="14px" />
           {error}
